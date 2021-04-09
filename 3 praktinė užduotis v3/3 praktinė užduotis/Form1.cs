21f37db843c45267f4e1eee3d2e0714423f952b7
JokubasM <65169105+JokubasM@users.noreply.github.com>
2021-04-09 12:48:14 +0300

    Add files via upload
    biski patobulintas failo saugojimas ir nuskaitymas

---
+++ 3 praktinė užduotis v3/3 praktinė užduotis/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace _3_praktinė_užduotis
+{
+    public partial class Form1 : Form
+    {
+        private RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(512);
+        private RSAParameters privatekey;
+        private RSAParameters publickey;
+        public Form1()
+        {
+            InitializeComponent();
+            publickey = RSA.ExportParameters(false);
+            privatekey = RSA.ExportParameters(true);
+        }
+
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            RSA.PersistKeyInCsp = false;
+            RSA.ImportParameters(publickey);
+            var data = Encoding.Unicode.GetBytes(textBox1.Text);
+            var cypher = RSA.Encrypt(data, false);
+            textBox2.Text = Convert.ToBase64String(cypher);
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+
+            FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fs);
+
+            sw.Write(textBox2.Text);
+
+            sw.Close();
+            fs.Close();
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            var dataBytes = Convert.FromBase64String(textBox2.Text);
+            RSA.ImportParameters(privatekey);
+            var result = RSA.Decrypt(dataBytes, false);
+            textBox4.Text = Encoding.Unicode.GetString(result);
+
+
+            
+          
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+
+            FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+
+            textBox2.Text = sr.ReadToEnd();
+
+            sr.Close();
+            fs.Close();
+        }
+    }
+}
